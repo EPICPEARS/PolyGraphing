c58af6f8ecf707659228e640db4ff1a763f8b6cb
EPICPEARS <fergusgrave@XxFortnitexX.local>
2022-08-16 23:29:15 +1000

    Clean Up Code

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PolyGraphing
 
-## PolyGraphing is a way to experiment quickly from the command line with polynominal functions
+
+### PolyGraphing is a way to experiment quickly from the command line with polynominal functions
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <main.hpp>
 
+void setup_axis(SDL_Renderer *rend){
+    SDL_RenderDrawLine(rend, 300, 0, 300, 480);
+    SDL_RenderDrawLine(rend, 0, 200, 640, 200);
+    SDL_SetRenderDrawColor(rend, 0, 0, 0, 100);
+    SDL_RenderPresent(rend);
+    SDL_SetRenderDrawColor(rend, 255, 255, 255, 100);
+    
+}
 
+void event_handling(SDL_Event e, SDL_bool *quit){
 
+    while (SDL_PollEvent(&e) != 0){
+            if (e.type == SDL_QUIT)
+            {
+            *quit = SDL_TRUE;
+            }
+            switch (e.type)
+            {
+                case SDL_KEYDOWN:
+                    *quit = SDL_TRUE;
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+}
 
 int main(int argc, char *argv[]){
-    
+
     if (argc <= 2){
         return 1;
     }
@@ -12,16 +38,14 @@ int main(int argc, char *argv[]){
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
-
-
     PolyNomialSolver psolve(argc, argv);
-    
-    
+
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_Log("failed to init: %s", SDL_GetError());
         return -1;
     }
+
     window = SDL_CreateWindow("Hello", 0, 0, 640, 480,SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
     
     renderer = SDL_CreateRenderer(window, -1, 0);
@@ -31,36 +55,18 @@ int main(int argc, char *argv[]){
     }
 
     SDL_bool quit = SDL_FALSE;
-    SDL_SetRenderDrawColor(renderer, 255, 20, 20, 100);
 
+
+    
     while (!quit){
         SDL_Event e;
 
         SDL_RenderClear(renderer);
-
         psolve.GetPoints(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 100);
+        setup_axis(renderer);
 
-        SDL_RenderDrawLine(renderer, 300, 0, 300, 480);
-        SDL_RenderDrawLineF(renderer, 0, 200, 640, 200);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 100);
-        SDL_RenderPresent(renderer);
-
-        while (SDL_PollEvent(&e) != 0){
-            if (e.type == SDL_QUIT)
-            {
-            quit = SDL_TRUE;
-            }
-            switch (e.type)
-            {
-            case SDL_KEYDOWN:
-                quit = SDL_TRUE;
-                break;
-            
-            default:
-                break;
-            }
-        }
+        event_handling(e, &quit);
+ 
     }
 
     SDL_Quit();
